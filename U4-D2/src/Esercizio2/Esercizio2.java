2fcab51e01ddcef2b750323dd01b0de942aeeb72
LuckyLukic <lucabjjiannice@gmail.com>
2023-06-28 15:11:11 +0200

    Esercizio2

--- U4-D2/src/Esercizio2/Esercizio2.java
+++ U4-D2/src/Esercizio2/Esercizio2.java
@@ -1,5 +1,48 @@
 package Esercizio2;
 
+import java.util.Scanner;
+
 public class Esercizio2 {
+	
+	public static void main(String[] args) {
+		
+		metodoSwitch();
+		
+		
+		
+		
+	}
+	
+	public static void metodoSwitch () {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Inserisci un umero da 0 a 10");
+		int numeroIntero = scanner.nextInt();
+		scanner.nextLine();
+		
+		switch (numeroIntero) {
+		
+		case 0 :
+			System.out.println("Zero");
+			break;
+			
+		case 1 :
+			System.out.println("Uno");
+			break;
+			
+		case 2 :
+			System.out.println("Due");
+			break;
+			
+		case 3 :
+			System.out.println("Tre");
+			break;	
+		
+		default:
+			System.out.println("Non abbiamo questo numero in memoria!");
+		    break;
+		}
+	}
 
 }
